ab6c6a66bea923120d0530c7c11154e1b7a3a199
darlzan <darlzan@foxmail.com>
2024-07-08 17:10:58 +0800

    system contracts: feature start

---
+++ README.md
@@ -0,0 +1,22 @@
+# Nero chain contracts
+
+This project contains the system built-in contracts of Nero chain.
+
+## Install Dependencies
+
+```
+npm install 
+```
+## Contract Compile
+
+```
+npm run compile
+```
+## Contract Test
+```
+npm run test
+```
+## Contract Test Coverage
+```
+npm run coverage
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,44 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-solpp";
+import "solidity-coverage";
+const prodConfig = {
+  Mainnet: true,
+}
+
+const devConfig = {
+  Mainnet: false,
+}
+
+const contractDefs: { [key: string]: object } = {
+  mainnet: prodConfig,
+  devnet: devConfig
+}
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  solpp: {
+    defs: contractDefs[process.env.NET]
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+        count: 100,
+        accountsBalance: "1000000000000000000000000000"
+      },
+      hardfork: "shanghai"
+    }
+  }
+};
+
+export default config;
