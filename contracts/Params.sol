cae09c47d13c61843c0bfe84a330019b3f6bc8a1
darlzan <darlzan@foxmail.com>
2024-09-14 15:09:22 +0800

    testnet config

--- contracts/Params.sol
+++ contracts/Params.sol
@@ -9,7 +9,7 @@ contract Params {
     address private constant engineCaller =
         address(0x000000000000000000004e65726F456e67696e65);
     // max active validators
-    uint8 public constant MaxValidators = 25;
+    uint8 public constant MaxValidators = 5;
 
     uint public constant MaxStakes = 200_000_000 * 1 ether; // max total stakes for a validator
     // min total stakes for a validator to be a valid candidate.
@@ -18,13 +18,13 @@ contract Params {
     uint public constant MinSelfStakes = 10_000_000 * 1 ether; // min self stakes for a user to register as a validator
     uint public constant StakeUnit = 1; // ether
 
-    uint public constant JailPeriod = 86400; // amount of blocks, about 3 days at 3 sec/block.
-    uint public constant UnboundLockPeriod = 21 days; // Seconds delay when a validator unbound staking.
+    uint public constant JailPeriod = 1200; // amount of blocks, about 1 hours at 3 sec/block.
+    uint public constant UnboundLockPeriod = 1 hours; // Seconds delay when a validator unbound staking.
     uint256 public constant PunishBase = 1000;
     uint256 public constant LazyPunishFactor = 5; // the punish factor when validator failed to propose blocks for specific times
     uint256 public constant EvilPunishFactor = 50; // the punish factor when a validator do something evil, such as "double sign".
-    uint256 public constant LazyPunishThreshold = 48; // accumulate amount of missing blocks for a validator to be punished
-    uint256 public constant DecreaseRate = 2; // the allowable amount of missing blocks in one epoch for each validator
+    uint256 public constant LazyPunishThreshold = 400; // accumulate amount of missing blocks for a validator to be punished
+    uint256 public constant DecreaseRate = 4; // the allowable amount of missing blocks in one epoch for each validator
 
     modifier onlyEngine() {
         require(msg.sender == engineCaller, "E40");
